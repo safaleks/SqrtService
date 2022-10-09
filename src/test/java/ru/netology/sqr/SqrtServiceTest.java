19965508c9c69552c91d1395e3676e310cb42d5c
safaleks <safaleks@mail.ru>
2022-10-09 20:52:10 +0500

    Deleted parameters from test

--- src/test/java/ru/netology/sqr/SqrtServiceTest.java
+++ src/test/java/ru/netology/sqr/SqrtServiceTest.java
@@ -7,19 +7,6 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 public class SqrtServiceTest {
 
     @ParameterizedTest
-/*
-    @CsvSource({
-            "3,200,300",
-            "90,99,9802",
-            "90,100,9801",
-            "1,100,100",
-            "1,9801,9801",
-            "88,101,9800",
-            "0,0,99",
-            "0,9802,15000"
-    })
-*/
-
     @CsvFileSource(files = "src/test/resources/square.scv")
     public void shouldExact(int expected, int min, int max) {
         SqrtService service = new SqrtService();
